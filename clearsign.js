8c925d49df5d6d8bf9a9a9d2b6a44fca35126106
arthur@24b6.net <arthur@24b6.net>
2013-09-16 13:22:05 -0400

    Adds: checking for bad signatures, some abstraction of the gnupg calls, starting to layout the index page

--- clearsign.js
+++ clearsign.js
@@ -1,136 +1,167 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * ClearSign
+ *
  */
 
-
-$(document).ready(function() {
-  $('.gpg-signed-text').clearsign();
-});
-
-
-
-$.fn.clearsign = function(options) {
-  return this.each(function () {
-
-  // Default options.
-  var settings = $.extend({
-    path : 'http://clearsign.me/',
-    cssPath : 'clearsign.css',
-    css : {
-      displayedText : 'gpg-displayed-text',
-      displayedStatus : 'gpg-displayed-status',
-      displayedRawText : 'gpg-raw-text'
-    },
-    gpgValidatorPath : 'clearsign.php'
-  }, options );
-
-  var element = this;
-
-  /**
-   * Load the CSS files to keep things simple.
-   */
-  function loadCSS(settings) {
-    // Only need to do this once.
-    if (! $('body').hasClass('clearsign')) {
-      var url = settings.path + settings.cssPath;
-      if (document.createStyleSheet) {
-        try { document.createStyleSheet(options.cssPath); } catch (e) { }
-      }
-      else {
-        var css;
-        css = document.createElement('link');
-        css.rel = 'stylesheet';
-        css.type = 'text/css';
-        css.media = "all";
-        css.href = url;
-        document.getElementsByTagName("head")[0].appendChild(css);
-      }
-      $('body').addClass('clearsign');
-    }
-  }
+/**
+ * Test to make sure that jQuery is loaded on the page. If it isn't load the
+ * local version.
+ */
+if (typeof jQuery=='undefined') {
+  var headTag = document.getElementsByTagName("head")[0];
+  var jqTag = document.createElement('script');
+  jqTag.type = 'text/javascript';
+  jqTag.src = 'jquery-1.10.1.min.js';
+  jqTag.onload = main;
+  headTag.appendChild(jqTag);
+}
+else {
+  main();
+}
 
 
-  /**
-   * Parse the signature out from the raw text.
-   */
-  function getSignature(text) {
-    var pattern = /SIGNATURE[-]*([\s\S]*?)[-]*END*/;
-    var matched = text.match(pattern);
-    return matched[1];
-  }
+// Code to be run after it is clear that jQuery is enabled
+function main() {
 
-
-  /**
-   * Parse the text out from the signed text.
-   */
-  function getSignedText(text) {
-    var pattern = /BEGIN[-]*[\s\S]*Hash.*([\s\S]*?)[-]*BEGIN/;
-    var matched = text.match(pattern);
-    return matched[1];
-  }
-
-
-  /**)
-   * Add the HTML for displaying the signature information.
-   */
-  function buildHTML() {
-    $(element).append('<div class="' + settings.css.displayedText + '" />');
-
-    $(element).append('<div class="' + settings.css.displayedRawText + '" />');
-    $('.' + settings.css.displayedRawText, element).wrapInner('<pre />');
-
-    $(element).append('<div class="' + settings.css.displayedStatus + '" />');
-    $('.' + settings.css.displayedStatus, element).append('<div class="data"></div><div class="status"><ul><li class="status">Status</li><li class="raw">Raw</li><li class="help">Help</li></ul></div>');
-
-    $('.' + settings.css.displayedStatus + ' li.raw', element).click(function () {
-      if ($(this).hasClass('open')) {
-        $(this).removeClass('open');
-        $('.' + settings.css.displayedRawText, element).hide(300);
-      }
-      else {
-        $(this).addClass('open');
-        $('.' + settings.css.displayedRawText, element).show(300);
-      }
-    });
-  }
-
-  /**
-   * Utility function to encode HTML entities.
-   */
-  function htmlEntityEncode(string) {
-    return $("<div/>").text(string).html();
-  }
-
-
-
-  // -----------------------------------------------------------
-
-
-
-  // Load the css.
-  loadCSS(settings)
-
-  // Get the input text.
-  var text = $(this).text();
-
-  // Remove existing markup.
-  $(this).html('');
-
-  // Build the clearsign markup.
-  buildHTML();
-
-
-  $('.' + settings.css.displayedText, this).html(getSignedText(text));
-  $('.' + settings.css.displayedRawText + ' pre', this).html(text);
-
-
-  var post = $.post(settings.path + settings.gpgValidatorPath, {signed_text : text }, function(data) {
-    console.dir(data);
-     $('.' + settings.css.displayedStatus + ' .data', element).html(htmlEntityEncode(data[0].name) + ' | ' + htmlEntityEncode(data[0].sign));
-   }, 'json');
-
-
-
+  // Enable on all signed text containers.
+  $(document).ready(function() {
+    $('.gpg-signed-text').clearsign();
   });
-};
+
+
+  // jQuery plugin.
+  $.fn.clearsign = function(options) {
+    return this.each(function () {
+
+    // Default options.
+    var settings = $.extend({
+      path : '/',
+      cssPath : 'clearsign.css',
+      css : {
+        displayedText : 'gpg-displayed-text',
+        displayedStatus : 'gpg-displayed-status',
+        displayedRawText : 'gpg-raw-text'
+      },
+      gpgValidatorPath : 'clearsign.php'
+    }, options );
+
+    var element = this;
+
+    /**
+     * Load the CSS files from javascript to keep implementation simple.
+     */
+    function loadCSS(settings) {
+      // Only need to do this once.
+      if (! $('body').hasClass('clearsign')) {
+        var url = settings.path + settings.cssPath;
+        if (document.createStyleSheet) {
+          try { document.createStyleSheet(options.cssPath); } catch (e) { }
+        }
+        else {
+          var css;
+          css = document.createElement('link');
+          css.rel = 'stylesheet';
+          css.type = 'text/css';
+          css.media = "all";
+          css.href = url;
+          document.getElementsByTagName("head")[0].appendChild(css);
+        }
+        $('body').addClass('clearsign');
+      }
+    }
+
+
+    /**
+     * Parse the signature out from the raw text.
+     */
+    function getSignature(text) {
+      var pattern = /SIGNATURE[-]*([\s\S]*?)[-]*END*/;
+      var matched = text.match(pattern);
+      return matched[1];
+    }
+
+
+    /**
+     * Parse the text out from the signed text.
+     */
+    function getSignedText(text) {
+      var pattern = /BEGIN[-]*[\s\S]*Hash.*([\s\S]*?)[-]*BEGIN/;
+      var matched = text.match(pattern);
+      return matched[1];
+    }
+
+
+    /**)
+     * Add the HTML for displaying the signature information.
+     */
+    function buildHTML() {
+      $(element).append('<div class="' + settings.css.displayedText + '" />');
+
+      $(element).append('<div class="' + settings.css.displayedRawText + '" />');
+      $('.' + settings.css.displayedRawText, element).wrapInner('<pre />');
+
+      $(element).append('<div class="' + settings.css.displayedStatus + '" />');
+      $('.' + settings.css.displayedStatus, element).append('<div class="data"></div><div class="status"><ul><li class="status">Status</li><li class="raw">Raw</li><li class="help">Help</li></ul></div>');
+
+      $('.' + settings.css.displayedStatus + ' li.raw', element).click(function () {
+        if ($(this).hasClass('open')) {
+          $(this).removeClass('open');
+          $('.' + settings.css.displayedRawText, element).hide(300);
+        }
+        else {
+          $(this).addClass('open');
+          $('.' + settings.css.displayedRawText, element).show(300);
+        }
+      });
+    }
+
+
+    /**
+     * Utility function to encode HTML entities.
+     */
+    function htmlEntityEncode(string) {
+      return $("<div/>").text(string).html();
+    }
+
+
+    // -----------------------------------------------------------
+
+
+
+    // Load the css.
+    loadCSS(settings)
+
+    // Get the input text.
+    var signed_text = $(this).text();
+
+    // Remove existing markup.
+    $(this).html('');
+
+    // Build the clearsign markup.
+    buildHTML();
+
+    // Populate the HTML with the signature parts.
+    $('.' + settings.css.displayedText, this).html(getSignedText(signed_text));
+    $('.' + settings.css.displayedRawText + ' pre', this).html(signed_text);
+
+    // Post the signed text to the verifier.
+    var post = $.post(settings.path + settings.gpgValidatorPath, {signed_text : signed_text }, function(data) {
+      console.log(data);
+      // Signature error condition.
+      if (typeof data.error !== 'undefined' && data.error) {
+        var string = data.error;
+        $(element).addClass('verification-failed');
+      }
+      else {
+        var string = htmlEntityEncode(data.name) + ' | ' + htmlEntityEncode(data.date) + ' | ' + data.id;
+        $(element).addClass('verified');
+      }
+      $('.' + settings.css.displayedStatus + ' .data', element).html(string);
+     }, 'json');
+
+
+
+    });
+  };
+
+} // main()
--- clearsign.php
+++ clearsign.php
@@ -1,22 +1,133 @@
 <?php
 
-// For now hand back some dummy text untill JS issues are worked out. 
-print json_encode(array(
-  array('name' => 'Jane Doe <jane@doe.com>',
-   'sign' => 'Jane Doe <thisisjane@doe.com>'
+/**
+ * @file
+ * Provides the PHP wrapper around GPG. Not implementing the pear version of
+ * gpg because it doesn't offer a verify option that can be used to look up
+ * the key id's identity.
+ *
+ */
 
- )));
 
-exit;
+/**
+ * Get the path to the GPG binary.
+ *
+ * @TODO need to do error checking if binary is not found.
+ * @TODO should check for gpg2 as well.
+ *
+ * @return string
+ */
+function get_gpg_path() {
+  static $path = FALSE;
+  if (! $path) {
+    $path = exec('export PATH=/usr/local/bin:/usr/bin:$PATH; which gpg');
+  }
+  return $path;
+}
 
-// $signed_text = escapeshellarg($_POST['signed_text']);
-print "<pre>";
 
+/**
+ * Fetches the key by running the clearsigned text through gpg.
+ *
+ * If this returns FALSE the signed text is not valid.
+ *
+ * @TODO these regexs need to be thought through.
+ * @TODO this should do some caching of keys in a database for performance.
+ *
+ * @param string $signed_text
+ *   Clear signed text to verify
+ * @return string
+ *   Key ID
+ */
+function get_key($signed_text) {
+  $gpg_path = get_gpg_path();
+
+  // Verify the signature.
+  exec("echo $signed_text | $gpg_path -n --verify 2>&1", $output, $ret);
+
+  // Make returned data easily matchable.
+  $output = implode(';', $output);
+
+  // Check for a bad signature.
+  $pattern = '/BAD signature/';
+  if (preg_match($pattern, $output)) {
+    $key['error'] = 'Bad signature';
+    return $key;
+  }
+
+  // Check for any valid key data that is achived on --verify.
+  $pattern = '/.*Signature made (.*) using.*key ID (.*?);.*Good signature from "(.*?)";/';
+  if (preg_match($pattern, $output, $matches)) {
+    $key['date'] = $matches[1];
+    $key['id'] = $matches[2];
+    $key['name'] = $matches[3];
+    return $key;
+  }
+
+  // No data, just find the key ID.
+  $pattern = "/.*key ID ([a-zA-z0-9]*)\;/";
+  if (preg_match($pattern, $output, $matches)) {
+    $key['id'] = $matches[1];
+    return $key;
+  }
+
+  // @TODO error handling.
+
+  // No valid data found.
+  return FALSE;
+}
+
+
+/**
+ * Looks up the key from a keyserver.
+ *
+ * @TODO handle error condition where key "XXXXXX" not found on keyserver
+ *
+ * @param string $key_id
+ *   Id of the key to look up
+ * @return array
+ *   Key information.
+ */
+function key_lookup($key) {
+  $gpg_path = get_gpg_path();
+
+  // The key has to have the 0x prepended to it to search on
+  $key_id = '0x' . $key['id'];
+
+  $command = get_gpg_path() . " --keyserver pgp.mit.edu  --search-keys $key_id";
+
+  $descriptorspec = array(
+    0 => array("pipe", "r"),
+    1 => array("pipe", "w")
+  );
+
+  $process = proc_open($command, $descriptorspec, $pipes);
+  if (is_resource($process)) {
+    list ($out, $in) = $pipes;
+    fwrite($out, "1\n");
+    $output = stream_get_contents($in);
+  }
+
+  // Output should be in the format of:
+  // (1)	name <email@email.net>
+	//  1024 bit DSA key KEYID, created: YYYY-MM-DD
+  $pattern = "/.*\).(.*)/";
+  if (preg_match($pattern, $output, $matches)) {
+    $key['name'] = $matches[1];
+    return $key;
+  }
+
+  return FALSE;
+}
+
+
+if (empty($_POST['signed_text'])) {
+// Dummy text to try.
 $signed_text =
 "-----BEGIN PGP SIGNED MESSAGE-----
 Hash: SHA1
 
-this is some test text
+this is some test text which will fail the verification.
 -----BEGIN PGP SIGNATURE-----
 Version: GnuPG v1.4.1 (GNU/Linux)
 
@@ -24,44 +135,28 @@ iD8DBQFDDA1eN5XoxaHnMrsRAsOPAKCBrt/YQuI8/rSD3rfQpYxJZowFPACdEYS5
 VoXLQNhyvirGLwvIZjuJLHY=
 =xGk+
 -----END PGP SIGNATURE-----";
-$signed_text = escapeshellarg($signed_text);
-
-$gpg_path = '/usr/local/bin/gpg';
-
-// Verify the signature.
-exec("echo $signed_text | $gpg_path -n --verify 2>&1", $output, $ret);
-
-print_r($output); print_r($ret);
-
-// Implode with an easily identifiable character.
-$output = implode('; ', $output);
-// Find the key ID.
-$pattern = "/.*key ID ([a-zA-z0-9]*)\;/";
-if (preg_match($pattern, $output, $matches)) {
-  $keys = array();
-
-  // Fetch the key from the key server.
-  exec($gpg_path . ' --search-keys ' . $matches[1] . ' 2>&1', $results, $ret);
-
-  foreach ($results as $count => $result) {
-
-    // Error lines start with "gpg:" and key lines start with "(X)".
-    if (strstr($result, 'gpg:') === FALSE) {
-
-      // Get the name associated with the key.
-      if (preg_match('/.*[0-9]*\)\s(.*)/', $result, $key_name)) {
-        // Get the key signing data from
-        preg_match('/\s*(.*)/', $results[$count + 1], $signature);
-        $keys[] = array('name' => $key_name[1], 'sign' => $signature[1]);
-      }
-    }
-  }
-
-  print json_encode($keys);
 }
 else {
-  print json_encode(array('error' => 'bad data'));
+  $signed_text = $_POST['signed_text'];
 }
+$signed_text = escapeshellarg($signed_text);
+
+
+if ($key = get_key($signed_text)) {
+  if (!empty($key['error']) || (!empty($key['name']) && !empty($key['date']))) {
+    print json_encode($key);
+  }
+  // Key was not found, look it up with the keyserver.
+  else if ($key = key_lookup($key)) {
+    print json_encode($key);
+  }
+}
+else {
+  // @TODO error condition
+  print json_encode($key);
+}
+
+
 
 
 
